ee0069b4a14fed9d31bee6cb55491324d4f30661
Taha <taha.visit@gmail.com>
2026-08-23 21:11:09 +0330

    feat: add docker and docker-compose

---
+++ docker/postgres/init/01-init.sql
@@ -0,0 +1,4 @@
+-- Creates the Keycloak database.
+-- The application database is created automatically via POSTGRES_DB.
+-- For production, create dedicated users per database instead of sharing POSTGRES_USER.
+CREATE DATABASE keycloak;
